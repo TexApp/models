02d0a323647a91af28136d3ee6bb51d933034b2c
Kyle Mitchell <kyleevanmitchell@gmail.com>
2013-01-12 04:21:21 -0600

    initial

---
+++ models.rb
@@ -0,0 +1,35 @@
+require 'data_mapper'
+
+class Case
+  include DataMapper::Resource
+  property :id, Serial
+  property :style, Text
+  property :court, Integer
+  property :filed, Date
+  property :number, String, :index => true
+  property :type, String
+
+  has n, :opinions
+end
+
+class Opinion
+  include DataMapper::Resource
+  property :id, Serial
+  property :number, String, :index => true
+  property :type, String
+  property :date, Date
+  property :url, Text
+  property :filename, String, :length => 75
+  
+  belongs_to :case
+end
+
+class Log
+  include DataMapper::Resource
+  property :id, Serial
+  property :court, Integer
+  property :date, Date
+end
+
+DataMapper::Model.raise_on_save_failure = true
+DataMapper.finalize
