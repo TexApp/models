30e0d3b93ab206e58ae3d5509b36c5c090a28f4d
Kyle Mitchell <kyleevanmitchell@gmail.com>
2013-01-14 21:24:10 -0600

    simplified

--- models.rb
+++ models.rb
@@ -3,11 +3,10 @@ require 'data_mapper'
 class Case
   include DataMapper::Resource
   property :id, Serial
-  property :style, Text
   property :court, Integer
+  property :docket_number, String, :index => true
   property :filed, Date
-  property :number, String, :index => true
-  property :type, String
+  property :style, Text
 
   has n, :opinions
 end
@@ -15,12 +14,10 @@ end
 class Opinion
   include DataMapper::Resource
   property :id, Serial
-  property :number, String, :index => true
-  property :type, String
   property :date, Date
+  property :type, String
   property :url, Text
-  property :filename, String, :length => 75
-  property :foreign_id, Integer
+  property :cloudfile, String, :length => 75
   
   belongs_to :case
 end
